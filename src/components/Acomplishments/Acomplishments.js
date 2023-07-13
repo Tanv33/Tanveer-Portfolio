3dc5233c8cdff55eb2301b564f60b72eabd37ec5
tanv33 <tanveer.khan2692000@gmail.com>
2023-07-13 17:31:24 +0500

    github followers api integrated for dynamic followers data

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,28 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
+import axios from "axios";
 
-const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 100, text: 'Students', },
-  { number: 45, text: 'Github Followers', },
-  { number: 29, text: 'Github Stars', }
+const defaultData = [
+  { number: 20, text: "Open Source Projects" },
+  { number: 100, text: "Students" },
+  { number: 45, text: "Github Followers" },
+  { number: 46, text: "Github Stars" },
 ];
 
-const Acomplishments = () => (
-  <Section>
-    <SectionTitle>Personal Achievements</SectionTitle>
-    <Boxes>
-      {data.map((card, index) => (
-        <Box key={index}>
-          <BoxNum>{`${card.number}+`}</BoxNum>
-          <BoxText>{card.text}</BoxText>
-        </Box>
-      ))}
-    </Boxes>
-    <SectionDivider/>
-  </Section>
-);
+const Acomplishments = () => {
+  const [data, setData] = useState(defaultData);
+
+  useEffect(() => {
+    (async () => {
+      const { data: userData } = await getFollowers();
+      // console.log(userData);
+      if (userData) {
+        const newFollowers = data.map((obj) => {
+          if (obj.text === "Github Followers") {
+            return { ...obj, number: userData.followers };
+          }
+          return obj;
+        });
+        setData(newFollowers);
+      }
+    })();
+  }, []);
+
+  return (
+    <Section>
+      <SectionTitle>Personal Achievements</SectionTitle>
+      <Boxes>
+        {data.map((card, index) => (
+          <Box key={index}>
+            <BoxNum>{`${card.number}+`}</BoxNum>
+            <BoxText>{card.text}</BoxText>
+          </Box>
+        ))}
+      </Boxes>
+      <SectionDivider />
+    </Section>
+  );
+};
 
 export default Acomplishments;
+
+const getFollowers = () => {
+  return axios.get(
+    "https://api.github.com/users/tanv33"
+    // "https://api.github.com/users/tanv33/followers?per_page=100"
+  );
+};
